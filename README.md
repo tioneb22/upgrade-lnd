4011101099de6b85e676980055f02e61764e1a18
tioneb22 <siege.storm@live.ca>
2022-11-06 15:23:34 -0500

    Added signature and hash checks

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # upgrade-lnd
 Code to automatically upgrade lnd (currently only for Debian on AMD64)
 
+## About
+Running it with the ```-f``` parameter will force it to download the latest version with any new commits.
+
+##  Parameters
+```-f``` Forcefully download and install the latest version.
+
 ## To Execute, run it with sudo:
 ```
-sudo ./upgrade-lnd.sh
+sudo ./upgrade-lnd.sh -f
 ```
 It will download the binaries to the "/tmp" directory. If the version is the same, it will delete the file and directory before downloading a fresh copy.
--- upgrade-lnd.sh
+++ upgrade-lnd.sh
@@ -1,40 +1,91 @@
 #!/bin/bash
 clear
 
-# initial setup
+
+# color code setup
 G='\033[0;32m' 		# green color
+Y='\033[1;33m' 		# yellow color
+R='\033[0;31m' 		# red color
+C='\033[1;36m' 		# cyan color
 NOCOLOR='\033[0m' 	# no color
-
-# define url strings and get new version info
-echo "Getting new version info."
-githubUrl="https://github.com"
-newPath=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "lnd-linux-amd64" | grep -Po "href=\"\K.*\.gz")
-newFile=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "lnd-linux-amd64" | grep -Po "href=\".*beta/\K.*\.gz")
-newFolder=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "lnd-linux-amd64" | grep -Po "href=\".*beta/\K.*beta")
-folderWStar="${newFolder}"'/*'
-newFullUrl=$githubUrl$newPath
-echo -e "${G}Done${NOCOLOR}"
-
-# Stop the LND service
-echo "Stopping the LND service."
-sudo systemctl stop lnd
-echo -e "${G}Done${NOCOLOR}"
-
-# Change to temp directory
 cd /tmp
 
-# Download then unpack tarball
-echo "Downloading and extracting tarball."
-rm -rf $newFolder 
-rm -rf $newFile
+
+# parameter setup
+while getopts f option
+do 
+    case "${option}"
+        in
+        f)force=0;;
+		?)print_help;;
+    esac
+done
+if ! [ -z "$force" ]; then
+	echo -e "Force flag was provided"
+fi
+
+
+# get version information
+echo -e "Getting current and new version info."
+currentVersion=$(lnd --version | grep -Po "commit=\K.*$")
+newVersion=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "lnd-linux-amd64" | grep -Po "download/\K.*/" | tr -d '/')
+rootDl="https://github.com/lightningnetwork/lnd/releases/download/"
+# will download only if new version available or if force (-f) flag was provided
+if [ "$currentVersion" == "$newVersion" ] && [ -z "$force" ]; then
+	echo -e "${G}Already at latest version.${NOCOLOR}"
+	exit 0
+else
+	echo -e "${Y}New major version available OR force parameter provided.${NOCOLOR}"
+fi
+
+
+# import and check gpg
+contributorSig=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "manifest" | grep -Po "manifest-guggero-.*\.sig" | awk 'NR==1')
+manifestTxt=$(/usr/bin/curl -s https://github.com/lightningnetwork/lnd/releases/ | grep "manifest" | grep -Po "manifest-v.*\.txt" | awk 'NR==1')
+wget -q $rootDl$newVersion/$contributorSig
+wget -q $rootDl$newVersion/$manifestTxt
+curl -s  https://raw.githubusercontent.com/lightningnetwork/lnd/master/scripts/keys/guggero.asc | gpg --import > /dev/null 2>&1
+gpgCheck=$(/usr/bin/gpg --verify $contributorSig $manifestTxt 2>&1 | grep -iq "Good signature " && echo $?)
+if [[ $gpgCheck ]]; then
+	echo -e "${G}Good Signature!${NOCOLOR}"
+	rm -rf $contributorSig
+else
+	echo -e "${R}Bad Signature!${NOCOLOR}"
+	rm -rf $contributorSig
+	exit 1
+fi
+
+
+# Download & verify file integrity then extract content
+echo -e "${Y}Downloading, verifying and extracting contents.${NOCOLOR}"
+newFullUrl=$rootDl$newVersion/lnd-linux-amd64-$newVersion.tar.gz
+newFolder=lnd-linux-amd64-$newVersion
+newFile=$newFolder.tar.gz
+rm -rf $newFolder && rm -rf $newFile
 wget -q $newFullUrl
+shaSumCheck=$(/usr/bin/sha256sum --check $manifestTxt --ignore-missing 2>&1 | grep -iq "OK" && echo $?)
+if [[ $shaSumCheck ]]; then
+	echo -e "${G}SHA256 hash check succeeded!${NOCOLOR}"
+else
+	echo -e "${R}Bad hash!${NOCOLOR}"
+	exit 1
+fi
+rm -rf $manifestTxt 
 tar -xzf $newFile
+
+
+# Install the new version
+# Stop the LND service
+echo -e "${Y}Stopping the LND service.${NOCOLOR}"
+sudo systemctl stop lnd
 echo -e "${G}Done${NOCOLOR}"
-
 # Install new LND version
+echo -e "${Y}Installing new binaries.${NOCOLOR}"
+folderWStar="${newFolder}"'/*'
 sudo install -m 0755 -o root -g root -t /usr/local/bin $folderWStar
-
+echo -e "${G}Done${NOCOLOR}"
 # Restart LND service
-echo "Starting the LND service."
+echo -e "${Y}Starting the LND service.${NOCOLOR}"
 sudo systemctl start lnd
 echo -e "${G}Done${NOCOLOR}"
+echo -e "${C}Successfully upgraded LND!${NOCOLOR}"
